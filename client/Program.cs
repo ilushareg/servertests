ebdad2dc42c89ef5d2085d5cc94acb61ddbe8b93
ilushareg <ilushareg@gmail.com>
2018-07-17 10:31:34 +0300

    added Client so it builds with VSCode

---
+++ client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Client console app staerted...");
+
+        }
+    }
+}
