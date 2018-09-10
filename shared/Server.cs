73dac42156db4968a39f3901d797ff1558af7484
ilushareg <ilushareg@gmail.com>
2018-09-10 11:25:27 +0300

    started shared client-server communication code

---
+++ shared/Server.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+
+namespace shared.Net
+{
+    
+    class Server
+    {
+        UdpClient client = null;
+
+        Server()
+        {
+
+        }
+
+        public void StartListening(int port)
+        {
+            if (client != null)
+                throw new Exception("Server port already creeated");
+
+            client = new UdpClient(port);
+
+
+
+        }
+
+    }
+}
